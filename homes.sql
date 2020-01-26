b16ee0d1b5c7b6666833d111c3383b64261edf1d
Jeong21 <59404244+Jeong21@users.noreply.github.com>
2020-01-26 09:23:57 -0600

    Update homes.sql

--- homes.sql
+++ homes.sql
@@ -5,18 +5,30 @@ CREATE TABLE IF NOT EXISTS homes (
     `State` VARCHAR(2) CHARACTER SET utf8,
     `Zip` INT,
     `Phone` VARCHAR(12) CHARACTER SET utf8,
-    `c` INT
+    `Address' VARCHAR(50) CHARACTER SET utf8
 );
 INSERT INTO homes VALUES
-    ('Our Savior''s Shelter',NULL,'Minneapolis','MN',55404,'612.872.4193',NULL),
-    ('St. Stephen''s Street Outreach',NULL,'Minneapolis','MN',55404,'612.874.0311',NULL),
-    ('House of Charity',NULL,'Minneapolis','MN',55404,'612.594.2000',NULL),
-    ('Simpson Housing Services',NULL,'Minneapolis','MN',55404,'612.874.8683',NULL),
-    ('Adult Shelter Connect MLPS',NULL,'Minneapolis','MN',55402,'612.248.2350',NULL),
-    ('People Serving People',NULL,'Minneapolis','MN',55415,'612.332.4500',NULL),
-    ('Salvation Army Harbor Light',NULL,'Minneapolis','MN',55403,'612.767.3100',NULL),
-    ('Catholic Charities Higher Ground Shelter',NULL,'Minneapolis','MN',55405,'612.294.8552',NULL),
-    ('Mary''s Place',NULL,'Minneapolis','MN',55405,'612.338.4855',NULL),
-    ('Evergreen Residence',NULL,'Minneapolis','MN',55405,'612.204.8406',NULL),
-    ('Sharing and Caring Hands',NULL,'Minneapolis','MN',55405,'612.338.4640',NULL),
-    ('Hope Street Shelter',NULL,'Minneapolis','MN',55407,'612.827.9372',NULL);
+    ('Our Savior''s Shelter',NULL,'Minneapolis','MN',55404,'612.872.4193','2219 Chicago Avenue South
+Minneapolis, MN 55404'),
+    ('St. Stephen''s Street Outreach',NULL,'Minneapolis','MN',55404,'612.874.0311','2309 Nicollet Avenue South
+Minneapolis, MN 55404'),
+    ('House of Charity',NULL,'Minneapolis','MN',55404,'612.594.2000','510 South 8th Street
+Minneapolis, MN 55404'),
+    ('Simpson Housing Services',NULL,'Minneapolis','MN',55404,'612.874.8683','2100 Pillsbury Avenue South
+Minneapolis, MN 55404'),
+    ('Adult Shelter Connect MLPS',NULL,'Minneapolis','MN',55402,'612.248.2350','215 South 8th Street
+Minneapolis, MN 55402'),
+    ('People Serving People',NULL,'Minneapolis','MN',55415,'612.332.4500','614 Third Street South
+Minneapolis, MN 55415'),
+    ('Salvation Army Harbor Light',NULL,'Minneapolis','MN',55403,'612.767.3100','1010 Currie Ave
+Minneapolis, MN 55403'),
+    ('Catholic Charities Higher Ground Shelter',NULL,'Minneapolis','MN',55405,'612.294.8552','165 Glenwood Avenue
+Minneapolis, MN 55405'),
+    ('Mary''s Place',NULL,'Minneapolis','MN',55405,'612.338.4855','401 North 7th St
+Minneapolis, MN 55405'),
+    ('Evergreen Residence',NULL,'Minneapolis','MN',55405,'612.204.8406','177 Glenwood Avenue North
+Minneapolis, MN 55405'),
+    ('Sharing and Caring Hands',NULL,'Minneapolis','MN',55405,'612.338.4640','525 North 7th St
+Minneapolis, MN 55405'),
+    ('Hope Street Shelter',NULL,'Minneapolis','MN',55407,'612.827.9372','1121 East 46th Street
+Minneapolis, MN 55407');
