772094df07ec6f8980870ddc8866112114eea1fb
Luca <39196460+lukfd@users.noreply.github.com>
2020-01-26 13:00:33 -0600

    Add files via upload

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -24,6 +24,7 @@ window.onload = function(){
     // $.getJSON(shelterapi + '/shelter?name'+app.searchbox, (data)=> {
 
     // });
+    /*
     var centermap = L.latLng(44.949642,-93.093124);
     var mymap = L.map('map', {
         center : centermap,
@@ -32,8 +33,25 @@ window.onload = function(){
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(mymap);
+    }).addTo(mymap);*/
     
-    var marker1 = L.marker([44.956758, -93.015139]).addTo(mymap).bindPopup('Information of the shelter').openPopup();
+    navigator.geolocation.getCurrentPosition(function(location) {
+        var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+      
+        var mymap = L.map('map').setView(latlng, 13)
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+          attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://mapbox.com">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox.streets',
+          accessToken: 'pk.eyJ1IjoiYmJyb29rMTU0IiwiYSI6ImNpcXN3dnJrdDAwMGNmd250bjhvZXpnbWsifQ.Nf9Zkfchos577IanoKMoYQ'
+        }).addTo(mymap);
+      
+        var marker = L.marker(latlng).addTo(mymap).bindPopup('YOU ARE HERE').openPopup();;
 
-};
+        var marker1 = L.marker([44.960232, -93.262482]).addTo(mymap).bindPopup('Our Saviours Housing Elca').openPopup();
+        var marker2 = L.marker([44.951235, -93.140184]).addTo(mymap).bindPopup('Jeremiah Program Saint Paul').openPopup();
+        var marker3 = L.marker([44.918415, -93.258739]).addTo(mymap).bindPopup('Hope Street Shelter').openPopup();
+
+    });
+
+ };   
