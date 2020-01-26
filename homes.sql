10cf3b5d21d828238334bf7b3e7335e082663da9
Jeong21 <59404244+Jeong21@users.noreply.github.com>
2020-01-26 01:20:16 -0600

    Created SQL File

---
+++ homes.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS homes (
+    `Name` VARCHAR(40) CHARACTER SET utf8,
+    `Column_2` INT,
+    `City` VARCHAR(11) CHARACTER SET utf8,
+    `State` VARCHAR(2) CHARACTER SET utf8,
+    `Zip` INT,
+    `Phone` VARCHAR(12) CHARACTER SET utf8,
+    `c` INT
+);
+INSERT INTO homes VALUES
+    ('Our Savior''s Shelter',NULL,'Minneapolis','MN',55404,'612.872.4193',NULL),
+    ('St. Stephen''s Street Outreach',NULL,'Minneapolis','MN',55404,'612.874.0311',NULL),
+    ('House of Charity',NULL,'Minneapolis','MN',55404,'612.594.2000',NULL),
+    ('Simpson Housing Services',NULL,'Minneapolis','MN',55404,'612.874.8683',NULL),
+    ('Adult Shelter Connect MLPS',NULL,'Minneapolis','MN',55402,'612.248.2350',NULL),
+    ('People Serving People',NULL,'Minneapolis','MN',55415,'612.332.4500',NULL),
+    ('Salvation Army Harbor Light',NULL,'Minneapolis','MN',55403,'612.767.3100',NULL),
+    ('Catholic Charities Higher Ground Shelter',NULL,'Minneapolis','MN',55405,'612.294.8552',NULL),
+    ('Mary''s Place',NULL,'Minneapolis','MN',55405,'612.338.4855',NULL),
+    ('Evergreen Residence',NULL,'Minneapolis','MN',55405,'612.204.8406',NULL),
+    ('Sharing and Caring Hands',NULL,'Minneapolis','MN',55405,'612.338.4640',NULL),
+    ('Hope Street Shelter',NULL,'Minneapolis','MN',55407,'612.827.9372',NULL);
